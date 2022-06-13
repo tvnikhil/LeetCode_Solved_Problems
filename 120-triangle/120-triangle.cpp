9a49f635c39a25981812274d242b1808fb977b64
Thanikella V Nikhil <83802691+tvnikhil@users.noreply.github.com>
2022-06-14 00:37:12 +0530

    Time: 16 ms (11.39%), Space: 8.7 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int ans(vector<vector<int>>& tr, int row, int col, vector<vector<int>> &dp) {
+    int dp[201][201];
+    int ans(vector<vector<int>>& tr, int row, int col) {
         if (dp[row][col] != -1)
             return dp[row][col];
-        if (row == tr.size()-1)
-            return tr[row][col];
-        if (col >= tr[row].size())
-            return INT_MAX;
-        int l = ans(tr, row+1, col, dp);
-        int r = ans(tr, row+1, col+1, dp);
-        dp[row][col] = min(l,r) + tr[row][col];
+        if (row == tr.size())
+            return 0;
+        int l = tr[row][col] + ans(tr, row+1, col);
+        int r = tr[row][col] + ans(tr, row+1, col+1);
+        dp[row][col] = min(l,r);
         return dp[row][col];
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        int m = triangle[n-1].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return ans(triangle, 0, 0, dp);
+        for (int i = 0; i < 201; i++)
+            for (int j = 0; j < 201; j++)
+                dp[i][j] = -1;
+        return ans(triangle, 0, 0);
     }
 };
