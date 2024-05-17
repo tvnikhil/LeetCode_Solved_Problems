78c73862b56018909d518d14aa46b8a832c67f93
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-05-17 16:40:37 -0500

    Time: 1 ms (100.00%), Space: 47.3 MB (30.56%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,32 +1,29 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        ArrayList<Integer> ansArr = new ArrayList<Integer>(nums.length);
+        int[] ansArr = new int[nums.length];
+        int last = nums.length-1;
         int p = 0, q = nums.length - 1;
         while (p <= q) {
             if (Math.abs(nums[p]) < Math.abs(nums[q])) {
-                ansArr.add(nums[q] * nums[q]);
+                ansArr[last--] = (nums[q] * nums[q]);
                 q--;
             }
             else if (Math.abs(nums[p]) > Math.abs(nums[q])) {
-                ansArr.add(nums[p] * nums[p]);
+                ansArr[last--] = (nums[p] * nums[p]);
                 p++;
             }
             else if (p == q) {
-                ansArr.add(nums[p] * nums[p]);
+                ansArr[last--] = nums[p] * nums[p];
                 p++;
                 q--;
             }
             else {
-                ansArr.add(nums[p] * nums[p]);
-                ansArr.add(nums[q] * nums[q]);
+                ansArr[last--] = (nums[p] * nums[p]);
+                ansArr[last--] = (nums[q] * nums[q]);
                 p++;
                 q--;
             }
         }
-        Collections.reverse(ansArr);
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = ansArr.get(i);
-        }
-        return nums;
+        return ansArr;
     }
 }
