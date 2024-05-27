d0c14d995b0836249f564c41878c1fb876aed5f4
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-05-27 03:14:00 -0500

    Time: 2 ms (47.70%), Space: 41 MB (83.20%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,22 +1,28 @@
+import java.util.Arrays;
+
 class Solution {
+    public int lowerBound(int[] arr, int target) {
+        int low = 0, high = arr.length - 1, ans = arr.length;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (target <= arr[mid]) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return ans;
+    }
+    
     public int specialArray(int[] nums) {
-        int max = -1;
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int i = 0; i <= 1000; i++)
-            hm.put(i, 0);
-        for (int i = 0; i < nums.length; i++) {
-            max = Math.max(max, nums[i]);
-            int temp = hm.get(nums[i]);
-            hm.put(nums[i], ++temp);
-        }
-        int[] cFreq = new int[max + 1];
-        cFreq[max] = hm.get(max);
-        for (int i = max-1; i >= 0; i--) {
-            cFreq[i] = hm.get(i) + cFreq[i + 1];
-        }
-        for (int i = 0; i <= max; i++) {
-            if (i == cFreq[i])
-                return cFreq[i];
+        // 0,0,3,4,4
+        Arrays.sort(nums);
+        for (int x = 0; x <= nums[nums.length - 1]; x++) {
+            int pos = lowerBound(nums, x);
+            int greaterElements = nums.length - pos;
+            if (x == greaterElements)
+                return x;
         }
         return -1;
     }
