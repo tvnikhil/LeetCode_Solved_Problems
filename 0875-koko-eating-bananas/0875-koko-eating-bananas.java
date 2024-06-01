a1d541ef526a1b4234b04e27f77aa872ed4c9c2c
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-05-31 20:15:13 -0500

    Time: 26 ms (37.90%), Space: 45 MB (54.31%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -18,12 +18,18 @@ class Solution {
         return true;
     }
     
+    public int sum(int[] arr) {
+        int res = 0;
+        for (int i = 0; i < arr.length; i++)
+            res += arr[i];
+        return res;
+    }
+    
     public int minEatingSpeed(int[] piles, int h) {
         int len = piles.length;
-        if (len == 1)
-            return (int) Math.ceil(piles[0] / (double) h);
         int maxi = getMax(piles);
-        int low = 1, high = maxi, ans = -1;
+        int low = (int) Math.ceil((double) sum(piles) / (double) h);
+        int high = maxi, ans = -1;
         while (low <= high) {
             int mid = (low + high) / 2;
             boolean hoursTaken = calculateHours(piles, mid, h);
