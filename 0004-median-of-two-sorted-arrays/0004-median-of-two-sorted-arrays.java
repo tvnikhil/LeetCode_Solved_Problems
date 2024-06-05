e40b050d97318f18bd5fd3bdbd4c9aa2b4ddda74
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-06-04 23:03:46 -0500

    Time: 2 ms (62.51%), Space: 45.7 MB (82.30%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,38 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int p1 = 0, p2 = 0, idx = 0;
-        int[] merged = new int[nums1.length + nums2.length];
+        int l = nums1.length + nums2.length;
+        int ele1 = -1, ele2 = -1, ele2Idx = l/2;
+        int ele1Idx = ele2Idx - 1;
         while (p1 < nums1.length && p2 < nums2.length) {
             if (nums1[p1] < nums2[p2]) {
-                merged[idx++] = nums1[p1];
+                if (idx == ele1Idx) ele1 = nums1[p1];
+                if (idx == ele2Idx) ele2 = nums1[p1];
                 p1++;
-            }
-            else if (nums1[p1] > nums2[p2]) {
-                merged[idx++] = nums2[p2];
-                p2++;
+                idx++;
             }
             else {
-                merged[idx++] = nums2[p2];
-                merged[idx++] = nums2[p2];
+                if (idx == ele1Idx) ele1 = nums2[p2];
+                if (idx == ele2Idx) ele2 = nums2[p2];
                 p2++;
-                p1++;
+                idx++;
             }
         }
         while (p1 < nums1.length) {
-            merged[idx++] = nums1[p1];
+            if (idx == ele1Idx) ele1 = nums1[p1];
+            if (idx == ele2Idx) ele2 = nums1[p1];
             p1++;
+            idx++;
         }
         while (p2 < nums2.length) {
-            merged[idx++] = nums2[p2];
+            if (idx == ele1Idx) ele1 = nums2[p2];
+            if (idx == ele2Idx) ele2 = nums2[p2];
             p2++;
+            idx++;
         }
-        int l = merged.length;
         double res = 0;
-        if (l % 2 == 0) {
-            res = (merged[l/2] + merged[(l/2)-1]) / (double) 2;
-        }
+        if (l % 2 == 0)
+            res = (ele1 + ele2) / (double) 2;
         else
-            res = merged[l/2];
+            res = ele2;
         return res;
     }
 }
