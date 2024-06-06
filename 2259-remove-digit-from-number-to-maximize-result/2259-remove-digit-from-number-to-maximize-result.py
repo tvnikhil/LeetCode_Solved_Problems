d1e4de306345a6fe3525f81a4fa473c43b8ca5c3
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-06-06 16:45:45 -0500

    Time: 39 ms (31.80%), Space: 16.5 MB (38.38%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -1,11 +1,22 @@
 class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
-        maxi = -1
-        for i in range(0, len(number)):
-            temp = -1
-            if i < len(number) - 1 and number[i] == digit:
-                temp = number[:i] + number[i + 1:]
-            elif i == len(number) - 1 and number[i] == digit:
-                temp = number[:i]
-            maxi = max(int(maxi), int(temp))
-        return str(maxi)
+        # maxi = -1
+        # for i in range(0, len(number)):
+        #     temp = -1
+        #     if i < len(number) - 1 and number[i] == digit:
+        #         temp = number[:i] + number[i + 1:]
+        #     elif i == len(number) - 1 and number[i] == digit:
+        #         temp = number[:i]
+        #     maxi = max(int(maxi), int(temp))
+        # return str(maxi)
+        toRemoveOccIdx = -1
+        for i in range(len(number)):
+            if i == len(number) - 1:
+                if number[i] == digit:
+                    return number[:i]
+            elif number[i] == digit and number[i + 1] > number[i]:
+                return number[:i] + number[i + 1:]
+            elif number[i] == digit:
+                toRemoveOccIdx = i
+        return number[:toRemoveOccIdx] + number[toRemoveOccIdx+1:]
+        
