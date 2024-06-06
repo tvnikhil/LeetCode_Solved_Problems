808c4d19eff191e2b26bfe9285ec436ad78a22d1
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-06-06 16:07:21 -0500

    Time: 39 ms (39.74%), Space: 16.4 MB (60.48%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,10 +1,12 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
         score = 0
-        asciiDict = {}
-        for i in range(ord("a"), ord("z") + 1):
-            asciiDict[chr(i)] = i
+        # asciiDict = {}
+        # for i in range(ord("a"), ord("z") + 1):
+        #     asciiDict[chr(i)] = i
+        # for i in range(len(s) - 1):
+        #     score += abs(asciiDict[s[i]] - asciiDict[s[i + 1]])
         for i in range(len(s) - 1):
-            score += abs(asciiDict[s[i]] - asciiDict[s[i + 1]])
+            score += abs(ord(s[i]) - ord(s[i + 1]))
         return score
         
