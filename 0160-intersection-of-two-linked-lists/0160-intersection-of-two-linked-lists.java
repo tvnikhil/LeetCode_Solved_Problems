9d84415ca53a20dbfd106a0f66cec5d11caad7d4
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-06-13 22:15:45 -0500

    Time: 1 ms (99.85%), Space: 48.5 MB (49.30%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,32 +10,20 @@
  * }
  */
 public class Solution {
-    public int returnLength(ListNode head) {
-        int len = 0;
-        ListNode temp = head;
-        while (temp != null) {
-            len++;
-            temp = temp.next;
-        }
-        return len;
-    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int lenA = returnLength(headA), lenB = returnLength(headB);
+        if (headA == null || headB == null)
+            return null;
         ListNode tempA = headA, tempB = headB;
-        if (lenA < lenB) {
-            for (int i = 0; i < lenB - lenA; i++)
-                tempB = tempB.next;
-        }
-        else if (lenA > lenB) {
-            for (int i = 0; i < lenA - lenB; i++)
-                tempA = tempA.next;
-        }
-        while (tempA != tempB && tempA != null && tempB != null) {
+        while (tempA != tempB) {
             tempA = tempA.next;
             tempB = tempB.next;
+            if (tempA == tempB)
+                return tempA;
+            if (tempA == null)
+                tempA = headB;
+            else if (tempB == null)
+                tempB = headA;
         }
-        if (tempA != null && tempB != null)
-            return tempA;
-        return null;
+        return tempA;
     }
 }
