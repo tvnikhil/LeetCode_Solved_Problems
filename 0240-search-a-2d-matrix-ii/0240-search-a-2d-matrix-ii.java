02292532d5e7da66d28de1f46cefe629d6cb0867
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-06-18 21:51:17 -0500

    Time: 6 ms (60.74%), Space: 46.2 MB (5.42%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,46 +1,42 @@
 class Solution {
-    public int floor(int[] arr, int target) {
-        int low = 0, high = arr.length - 1, ans = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] > target)
-                high = mid - 1;
-            else {
-                ans = mid;
-                low = mid + 1;
-            }
-        }
-        return ans;
-    }
-    
-    public int ceil(int[] arr, int target) {
-        int low = 0, high = arr.length - 1, ans = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] < target)
-                low = mid + 1;
-            else {
-                ans = mid;
-                high = mid - 1;
-            }
-        }
-        return ans;
-    }
-        
     public boolean searchMatrix(int[][] matrix, int target) {
-        int right = floor(matrix[0], target);
+        int low = 0, high = matrix[0].length - 1, right = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (matrix[0][mid] > target)
+                high = mid - 1;
+            else {
+                right = mid;
+                low = mid + 1;
+            }
+        }
         if (right == -1)
             return false;
-        int left = ceil(matrix[matrix.length - 1], target);
+        int left = -1;
+        low = 0; 
+        high = matrix[0].length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (matrix[matrix.length - 1][mid] < target)
+                low = mid + 1;
+            else {
+                left = mid;
+                high = mid - 1;
+            }
+        }
         if (left == -1)
             return false;
         for (int j = left; j <= right; j++) {
-            int[] arr = new int[matrix.length];
-            for (int i = 0; i < arr.length; i++)
-                arr[i] = matrix[i][j];
-            int check = floor(arr, target);
-            if (check != -1 && matrix[check][j] == target)
-                return true;
+            low = 0; high = matrix.length - 1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (matrix[mid][j] == target)
+                    return true;
+                else if (matrix[mid][j] > target)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
         }
         return false;
     }
