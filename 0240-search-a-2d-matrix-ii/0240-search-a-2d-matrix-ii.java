3c96892cb37ae7ac647cadd6d5c19d64d3ee722f
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-06-18 22:12:23 -0500

    Time: 7 ms (22.82%), Space: 45.8 MB (50.71%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,43 +1,54 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int low = 0, high = matrix[0].length - 1, right = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (matrix[0][mid] > target)
-                high = mid - 1;
-            else {
-                right = mid;
-                low = mid + 1;
-            }
-        }
-        if (right == -1)
-            return false;
-        int left = -1;
-        low = 0; 
-        high = matrix[0].length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (matrix[matrix.length - 1][mid] < target)
-                low = mid + 1;
-            else {
-                left = mid;
-                high = mid - 1;
-            }
-        }
-        if (left == -1)
-            return false;
-        for (int j = left; j <= right; j++) {
-            low = 0; high = matrix.length - 1;
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (matrix[mid][j] == target)
-                    return true;
-                else if (matrix[mid][j] > target)
-                    high = mid - 1;
-                else
-                    low = mid + 1;
-            }
+        int rowLength = matrix.length, colLength = matrix[0].length;
+        int row = 0, col = colLength - 1;
+        while (row < rowLength && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            if (matrix[row][col] > target)
+                col--;
+            else
+                row++;
         }
         return false;
+        // int low = 0, high = matrix[0].length - 1, right = -1;
+        // while (low <= high) {
+        //     int mid = (low + high) / 2;
+        //     if (matrix[0][mid] > target)
+        //         high = mid - 1;
+        //     else {
+        //         right = mid;
+        //         low = mid + 1;
+        //     }
+        // }
+        // if (right == -1)
+        //     return false;
+        // int left = -1;
+        // low = 0; 
+        // high = matrix[0].length - 1;
+        // while (low <= high) {
+        //     int mid = (low + high) / 2;
+        //     if (matrix[matrix.length - 1][mid] < target)
+        //         low = mid + 1;
+        //     else {
+        //         left = mid;
+        //         high = mid - 1;
+        //     }
+        // }
+        // if (left == -1)
+        //     return false;
+        // for (int j = left; j <= right; j++) {
+        //     low = 0; high = matrix.length - 1;
+        //     while (low <= high) {
+        //         int mid = (low + high) / 2;
+        //         if (matrix[mid][j] == target)
+        //             return true;
+        //         else if (matrix[mid][j] > target)
+        //             high = mid - 1;
+        //         else
+        //             low = mid + 1;
+        //     }
+        // }
+        // return false;
     }
 }
