06dc7c65a8850ebd9424a0b525d2050c8f254da7
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-06-20 14:42:04 -0500

    Time: 42 ms (79.45%), Space: 55.8 MB (52.54%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -1,22 +1,24 @@
 class Solution {
-    public int calcBallsCnt(int[] arr, int gap) {
+    public int calcBallsCnt(int[] arr, int gap, int maxiCnt) {
         int nextPossiblePlace = arr[0] + gap, ballsCnt = 1;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] >= nextPossiblePlace) {
                 ballsCnt++;
                 nextPossiblePlace = arr[i] + gap;
             }
+            if (ballsCnt > maxiCnt)
+                return -1;
         }
         return ballsCnt;
     }
     
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
-        int low = 1, high = position[position.length - 1], ans = -1;
+        int low = 1, high = position[position.length - 1] - position[0], ans = -1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            int ballsCnt = calcBallsCnt(position, mid);
-            if (ballsCnt < m)
+            int ballsCnt = calcBallsCnt(position, mid, m);
+            if (ballsCnt != -1 && ballsCnt < m)
                 high = mid - 1;
             else {
                 ans = mid;
