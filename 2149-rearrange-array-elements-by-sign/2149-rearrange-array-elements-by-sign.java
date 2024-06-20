da8be0a53cecc3b4359f61060bcf12c21b94d607
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-06-20 16:17:48 -0500

    Time: 7 ms (25.59%), Space: 76.8 MB (66.46%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,26 +1,38 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] pos = new int[nums.length / 2];
-        int[] neg = new int[nums.length / 2];
-        int p1 = 0, p2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0)
-                pos[p1++] = nums[i];
-            else
-                neg[p2++] = nums[i];
+        int[] res = new int[nums.length];
+        int resPtr = 0;
+        int posPtr = 0, negPtr = 0;
+        while (posPtr < nums.length && negPtr < nums.length) {
+            while (posPtr < nums.length && nums[posPtr] < 0) posPtr++;
+            while (negPtr < nums.length && nums[negPtr] > 0) negPtr++;
+            res[resPtr++] = nums[posPtr];
+            res[resPtr++] = nums[negPtr];
+            posPtr++;
+            negPtr++;
         }
-        int choice = 0;
-        p1 = 0; p2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (choice == 0) {
-                nums[i] = pos[p1++];
-                choice = 1;
-            }
-            else {
-                nums[i] = neg[p2++];
-                choice = 0;
-            }
-        }
-        return nums;
+        return res;
+        // int[] pos = new int[nums.length / 2];
+        // int[] neg = new int[nums.length / 2];
+        // int p1 = 0, p2 = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] > 0)
+        //         pos[p1++] = nums[i];
+        //     else
+        //         neg[p2++] = nums[i];
+        // }
+        // int choice = 0;
+        // p1 = 0; p2 = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (choice == 0) {
+        //         nums[i] = pos[p1++];
+        //         choice = 1;
+        //     }
+        //     else {
+        //         nums[i] = neg[p2++];
+        //         choice = 0;
+        //     }
+        // }
+        // return nums;
     }
 }
