503c29d2e0772d0aa5cab3afae574397cb8ad3ca
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-07 19:29:10 -0500

    Time: 62 ms (10.63%), Space: 16.5 MB (73.42%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkValid(self, ip):
+        l = ip.split(".")
+        for numStr in l:
+            if numStr == "" or numStr == " ":
+                return False
+            num = int(numStr)
+            if len(numStr) > 1 and numStr[0] == "0" and num >= 0:
+                return False
+            if num > 255:
+                return False
+        return True
+    
+    def generate(self, s, idx, currIp, dotCnt, ans):
+        if dotCnt == 3 and self.checkValid(currIp + s[idx:]):
+            ans.append(str(currIp + s[idx:]))
+            return
+        if dotCnt > 3 or idx >= len(s):
+            return
+        currIp += s[idx]
+        self.generate(s, idx+1, currIp+".", dotCnt+1, ans)
+        self.generate(s, idx+1, currIp, dotCnt, ans)
+    
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        ans = []
+        idx = 0
+        currIp = ""
+        dotCnt = 0
+        self.generate(s, idx, currIp, dotCnt, ans)
+        return ans
