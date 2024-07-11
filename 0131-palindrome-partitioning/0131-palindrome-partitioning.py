4a6862af91a4e0863ff964cd8636d3d41a72e530
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-11 16:58:49 -0500

    Time: 463 ms (60.25%), Space: 32.2 MB (80.10%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,4 +1,12 @@
 class Solution:
+    def check(self, s):
+        i, j = 0, len(s)-1
+        while i <= j:
+            if s[i] != s[j]:
+                return False
+            i, j = i+1, j-1
+        return True
+    
     def dfs(self, s, palList, ans):
         if len(s) == 0:
             ans.append(list(palList))
@@ -6,7 +14,7 @@ class Solution:
         for length in range(1, len(s)+1):
             left = s[0:length]
             right = s[length:]
-            if left == left[::-1]:
+            if self.check(left):
                 palList.append(str(left))
                 self.dfs(right, palList, ans)
                 palList.pop()
