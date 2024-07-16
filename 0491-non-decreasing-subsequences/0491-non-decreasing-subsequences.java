1e7ea3f7365cd0edf03f72bd7e301fb6ea7988e5
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-16 08:55:06 -0500

    Time: 6 ms (87.66%), Space: 50.8 MB (73.55%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,25 +1,27 @@
 class Solution {
-    void dfs(int[] nums, int idx, List<Integer> subseq, Set<List<Integer>> hs) {
+    void dfs(int[] nums, int idx, List<Integer> subseq, List<List<Integer>> ans) {
         if (subseq.size() >= 2) {
-            hs.add(new ArrayList<>(subseq));
+            ans.add(new ArrayList<>(subseq));
         }
+        Set<Integer> hs = new HashSet<>();
         for (int i = idx; i < nums.length; i++) {
+            if (hs.contains(nums[i]))
+                    continue;
             if (subseq.size() == 0 || 
             (subseq.size() > 0 && nums[i] >= subseq.get(subseq.size()-1))) {
+                hs.add(nums[i]);
                 subseq.add(nums[i]);
-                dfs(nums, i+1, subseq, hs);
+                dfs(nums, i+1, subseq, ans);
                 subseq.remove(subseq.size()-1);
             }
-            else
-                dfs(nums, i+1, subseq, hs);
         }
     }
     
     public List<List<Integer>> findSubsequences(int[] nums) {
         List<Integer> subseq = new ArrayList<>();
-        Set<List<Integer>> hs = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         int idx = 0;
-        dfs(nums, idx, subseq, hs);
-        return new ArrayList<>(hs);
+        dfs(nums, idx, subseq, ans);
+        return ans;
     }
 }
