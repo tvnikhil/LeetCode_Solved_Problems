e516279ff2cee0d257704ad393ecae069864d518
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-17 07:40:06 -0500

    Time: 0 ms (100.00%), Space: 41.8 MB (5.66%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,16 +14,30 @@
  * }
  */
 class Solution {
+    // void getPreOrder(TreeNode root, List<Integer> preOrder) {
+    //     if (root == null)
+    //         return;
+    //     preOrder.add(root.val);
+    //     getPreOrder(root.left, preOrder);
+    //     getPreOrder(root.right, preOrder);
+    // }
+    
     void getPreOrder(TreeNode root, List<Integer> preOrder) {
-        if (root == null)
-            return;
-        preOrder.add(root.val);
-        getPreOrder(root.left, preOrder);
-        getPreOrder(root.right, preOrder);
+        Deque<TreeNode> dq = new LinkedList<>();
+        dq.offerFirst(root);
+        while (dq.size() > 0) {
+            TreeNode node = dq.pollFirst();
+            preOrder.add(node.val);
+            if (node.right != null)
+                dq.offerFirst(node.right);
+            if (node.left != null)
+                dq.offerFirst(node.left);
+        }
     }
     
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> preOrder = new ArrayList<>();
+        if (root == null) return preOrder;
         getPreOrder(root, preOrder);
         return preOrder;
     }
