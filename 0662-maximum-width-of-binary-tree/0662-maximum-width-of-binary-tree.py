58ed215522e982e7b66ea7d1195b8a2f36c269b8
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-23 07:09:01 -0500

    Time: 45 ms (46.33%), Space: 17.3 MB (43.10%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -5,8 +5,8 @@
 #         self.left = left
 #         self.right = right
 class Pair:
-    def __init__(self, TreeNode, level=0):
-        self.level = level
+    def __init__(self, TreeNode, idx=0):
+        self.idx = idx
         self.node = TreeNode
 
 class Solution:
@@ -15,16 +15,14 @@ class Solution:
         queue = [p]
         maxWidth = 0
         while len(queue):
-            level = []
+            maxWidth = max(maxWidth, queue[-1].idx-queue[0].idx+1)
             currSize = len(queue)
             for i in range(currSize):
                 curr = queue[0]
                 queue.pop(0)
-                level.append(curr)
                 if curr.node.left:
-                    queue.append(Pair(curr.node.left, 2 * (curr.level) + 1))
+                    queue.append(Pair(curr.node.left, 2 * (curr.idx) + 1))
                 if curr.node.right:
-                    queue.append(Pair(curr.node.right, 2 * (curr.level) + 2))
-            maxWidth = max(maxWidth, level[-1].level-level[0].level+1)
+                    queue.append(Pair(curr.node.right, 2 * (curr.idx) + 2))
         return maxWidth
         
