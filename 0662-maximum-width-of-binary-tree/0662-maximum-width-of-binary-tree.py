5d977c2caab90b930e628143c01271c27f25c25a
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-23 07:16:34 -0500

    Time: 36 ms (92.09%), Space: 17.2 MB (43.10%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -16,13 +16,14 @@ class Solution:
         maxWidth = 0
         while len(queue):
             maxWidth = max(maxWidth, queue[-1].idx-queue[0].idx+1)
+            leftIdx = queue[0].idx
             currSize = len(queue)
             for i in range(currSize):
                 curr = queue[0]
                 queue.pop(0)
                 if curr.node.left:
-                    queue.append(Pair(curr.node.left, 2 * (curr.idx) + 1))
+                    queue.append(Pair(curr.node.left, 2 * (curr.idx-leftIdx) + 1))
                 if curr.node.right:
-                    queue.append(Pair(curr.node.right, 2 * (curr.idx) + 2))
+                    queue.append(Pair(curr.node.right, 2 * (curr.idx-leftIdx) + 2))
         return maxWidth
         
