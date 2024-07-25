32e092c54fda9f4547d08e5ba866b363a03755ba
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-25 18:35:01 -0500

    Time: 49 ms (49.10%), Space: 16.5 MB (36.04%) - LeetHub

--- 0842-split-array-into-fibonacci-sequence/0842-split-array-into-fibonacci-sequence.py
+++ 0842-split-array-into-fibonacci-sequence/0842-split-array-into-fibonacci-sequence.py
@@ -1,5 +1,5 @@
 class Solution:
-    def dfs(self, num, seq):
+    def dfs(self, num, seq, lim):
         if len(seq) > 2:
             if (seq[-1]) != (seq[-2]) + (seq[-3]): return []
         if len(num) == 0 and len(seq) >= 3:
@@ -8,15 +8,16 @@ class Solution:
             left = num[0:i+1]
             if len(left) > 1 and left[0] == "0":
                 continue
-            if int(left) >= 2**31:
+            if int(left) >= lim:
                 break
             seq.append(int(left))
-            if self.dfs(num[i+1:], seq) != []: return seq
+            if self.dfs(num[i+1:], seq, lim) != []: return seq
             seq.pop()
         return []
     
     def splitIntoFibonacci(self, num: str) -> List[int]:
+        lim = 2**31
         seq = []
-        seq = self.dfs(num, seq)
+        seq = self.dfs(num, seq, lim)
         return seq
         
