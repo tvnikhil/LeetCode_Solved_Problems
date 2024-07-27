859a1e7af5709b9b5a29eba10404cf548f774e70
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-27 13:26:32 -0500

    Time: 0 ms (100.00%), Space: 41.5 MB (38.22%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -22,23 +22,44 @@ class Solution {
     //     getPreOrder(root.right, preOrder);
     // }
     
-    void getPreOrder(TreeNode root, List<Integer> preOrder) {
-        Deque<TreeNode> dq = new LinkedList<>();
-        dq.offerFirst(root);
-        while (dq.size() > 0) {
-            TreeNode node = dq.pollFirst();
-            preOrder.add(node.val);
-            if (node.right != null)
-                dq.offerFirst(node.right);
-            if (node.left != null)
-                dq.offerFirst(node.left);
-        }
-    }
+    // void getPreOrder(TreeNode root, List<Integer> preOrder) {
+    //     Deque<TreeNode> dq = new LinkedList<>();
+    //     dq.offerFirst(root);
+    //     while (dq.size() > 0) {
+    //         TreeNode node = dq.pollFirst();
+    //         preOrder.add(node.val);
+    //         if (node.right != null)
+    //             dq.offerFirst(node.right);
+    //         if (node.left != null)
+    //             dq.offerFirst(node.left);
+    //     }
+    // }
     
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> preOrder = new ArrayList<>();
-        if (root == null) return preOrder;
-        getPreOrder(root, preOrder);
+        // if (root == null) return preOrder;
+        // getPreOrder(root, preOrder);
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left == null) {
+                preOrder.add(curr.val);
+                curr = curr.right;
+            }
+            else {
+                TreeNode temp = curr.left;
+                while (temp.right != null && temp.right != curr)
+                    temp = temp.right;
+                if (temp.right == null) {
+                    preOrder.add(curr.val);
+                    temp.right = curr;
+                    curr = curr.left;
+                }
+                else {
+                    temp.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
         return preOrder;
     }
 }
