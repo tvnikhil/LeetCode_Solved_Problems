a065186990bf4d98ed78c9da6255b103556cff35
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-07-27 13:20:43 -0500

    Time: 0 ms (100.00%), Space: 41.5 MB (38.13%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,32 +14,53 @@
  * }
  */
 class Solution {
-    void getInOrder(TreeNode root, List<Integer> inOrder) {
-        // if (root == null)
-        //     return;
-        // getInOrder(root.left, inOrder);
-        // inOrder.add(root.val);
-        // getInOrder(root.right, inOrder);
-        Stack<TreeNode> stk = new Stack<>();
-        TreeNode node = root;
-        while (true) {
-            if (node != null) {
-                stk.push(node);
-                node = node.left;
-            }
-            else if (node == null && stk.isEmpty() == true)
-                break;
-            else {
-                node = stk.pop();
-                inOrder.add(node.val);
-                node = node.right;
-            }
-        }
-    }
+    // void getInOrder(TreeNode root, List<Integer> inOrder) {
+    //     // if (root == null)
+    //     //     return;
+    //     // getInOrder(root.left, inOrder);
+    //     // inOrder.add(root.val);
+    //     // getInOrder(root.right, inOrder);
+    //     Stack<TreeNode> stk = new Stack<>();
+    //     TreeNode node = root;
+    //     while (true) {
+    //         if (node != null) {
+    //             stk.push(node);
+    //             node = node.left;
+    //         }
+    //         else if (node == null && stk.isEmpty() == true)
+    //             break;
+    //         else {
+    //             node = stk.pop();
+    //             inOrder.add(node.val);
+    //             node = node.right;
+    //         }
+    //     }
+    // }
     
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> inOrder = new ArrayList<>();
-        getInOrder(root, inOrder);
+        // getInOrder(root, inOrder);
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left == null) {
+                inOrder.add(curr.val);
+                curr = curr.right;
+            }
+            else {
+                TreeNode temp = curr.left;
+                while (temp.right != null && temp.right != curr)
+                    temp = temp.right;
+                if (temp.right == null) {
+                    temp.right = curr;
+                    curr = curr.left;
+                }
+                else {
+                    inOrder.add(curr.val);
+                    temp.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
         return inOrder;
     }
 }
