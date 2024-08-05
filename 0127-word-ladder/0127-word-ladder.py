3b6720e7c0e8cd31f551615b9848993154ed4bf7
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-08-05 17:40:51 -0500

    Time: 408 ms (33.46%), Space: 17.8 MB (87.20%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,33 @@
+class Solution:
+    def getPossibilities(self, word, vis, letters):
+        ans = []
+        for i in range(0, len(word)):
+            for j in letters[i]:
+                temp = word[0:i] + j + word[i+1:]
+                if temp != word and temp in vis and vis[temp] == 0:
+                    ans.append(temp)
+        return ans
+    
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        vis,letters = {},{}
+        for i in range(0, len(beginWord)):
+            letters[i] = set()
+        for i in wordList:
+            vis[i] = 0
+            for j in range(0, len(i)):
+                letters[j].add(i[j])
+        que = [beginWord]
+        ans = 1
+        vis[beginWord] = 1
+        while len(que) != 0:
+            size = len(que)
+            for s in range(size):
+                word = que.pop(0)
+                if word == endWord:
+                    return ans
+                pos = self.getPossibilities(word, vis, letters)
+                for p in pos:
+                    que.append(p)
+                    vis[p] = 1
+            ans += 1
+        return 0
