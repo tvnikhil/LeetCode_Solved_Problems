30a8956dce71119611cdc0896070d1381c0ffa56
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-09-01 12:02:30 -0500

    Time: 72 ms (41.80%), Space: 19.5 MB (8.20%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -5,28 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findParents(self, root, parent, hm):
-        if not root:
-            return
-        hm[root] = parent
-        self.findParents(root.left, root, hm)
-        self.findParents(root.right, root, hm)
+    # def findParents(self, root, parent, hm):
+    #     if not root:
+    #         return
+    #     hm[root] = parent
+    #     self.findParents(root.left, root, hm)
+    #     self.findParents(root.right, root, hm)
     
     def sumEvenGrandparent(self, root: TreeNode) -> int:
         hm = {}
-        self.findParents(root, None, hm)
+        # self.findParents(root, None, hm)
         que = deque([root])
         ans = 0
+        hm[root] = None
+        
         while que:
             node = que.popleft()
-            p = hm[node]
-            if p is not None:
-                gp = hm[p]
-                if gp is not None and gp.val % 2 == 0:
-                    ans += node.val
             if node.left:
                 que.append(node.left)
+                gp = hm[node]
+                if gp is not None and gp.val % 2 == 0:
+                    ans += node.left.val
+                hm[node.left] = node
             if node.right:
                 que.append(node.right)
+                gp = hm[node]
+                if gp is not None and gp.val % 2 == 0:
+                    ans += node.right.val
+                hm[node.right] = node
         return ans
         
