c1a426ee8e4daced545e4405571a08b247eea26b
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-09-19 13:37:34 -0500

    Time: 57 ms (81.94%), Space: 19.7 MB (15.54%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -9,21 +9,36 @@ class Solution:
         
 #         return dp[(x,y)]
     
+    def dfs(self, x, y, tri, dp):
+        # f(x,y) -> has the min time to go from that position to the last row
+        if x == len(tri)-1:
+            return tri[x][y]
+        if (x,y) in dp:
+            return dp[(x,y)]
+        left = self.dfs(x+1,y,tri,dp)
+        right = self.dfs(x+1,y+1,tri,dp)
+        
+        dp[(x,y)] = min(left, right) + tri[x][y]
+        return dp[(x,y)]
+    
     def minimumTotal(self, tri: List[List[int]]) -> int:
         m = len(tri)
-        dp = [[0 for j in range(i+1)] for i in range(len(tri))]
-        dp[0][0] = tri[0][0]
+        dp = {}
+        return self.dfs(0,0,tri,dp)
         
-        for i in range(1,len(dp)):
-            for j in range(i+1):
-                pos1, pos2 = float("inf"), float("inf")
-                if j <= i-1:
-                    pos1 = min(pos1, dp[i-1][j])
-                if j-1 >= 0:
-                    pos2 = min(pos2, dp[i-1][j-1])
-                dp[i][j] = min(pos1, pos2) + tri[i][j]
+#         dp = [[0 for j in range(i+1)] for i in range(len(tri))]
+#         dp[0][0] = tri[0][0]
         
-        return min(dp[len(dp)-1])
+#         for i in range(1,len(dp)):
+#             for j in range(i+1):
+#                 pos1, pos2 = float("inf"), float("inf")
+#                 if j <= i-1:
+#                     pos1 = min(pos1, dp[i-1][j])
+#                 if j-1 >= 0:
+#                     pos2 = min(pos2, dp[i-1][j-1])
+#                 dp[i][j] = min(pos1, pos2) + tri[i][j]
+        
+#         return min(dp[len(dp)-1])
                 
         
 #         ans = float("inf")
