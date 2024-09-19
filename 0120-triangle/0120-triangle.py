d444276ef98ebb12682db9ddd765b24c5d7acecf
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-09-18 19:21:15 -0500

    Time: 71 ms (13.91%), Space: 18 MB (25.01%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,37 @@
+class Solution:
+#     def dfs(self, x, y, tri,dp):
+#         if x < 0 or y < 0 or y > x:
+#             return float("inf")
+#         if (x,y) in dp:
+#             return dp[(x,y)]
+        
+#         dp[(x,y)] = min(self.dfs(x-1,y,tri,dp), self.dfs(x-1,y-1,tri,dp)) + tri[x][y]
+        
+#         return dp[(x,y)]
+    
+    def minimumTotal(self, tri: List[List[int]]) -> int:
+        m = len(tri)
+        dp = [[0 for j in range(i+1)] for i in range(len(tri))]
+        dp[0][0] = tri[0][0]
+        
+        for i in range(1,len(dp)):
+            for j in range(i+1):
+                pos1, pos2 = float("inf"), float("inf")
+                if j <= i-1:
+                    pos1 = min(pos1, dp[i-1][j])
+                if j-1 >= 0:
+                    pos2 = min(pos2, dp[i-1][j-1])
+                dp[i][j] = min(pos1, pos2) + tri[i][j]
+        
+        return min(dp[len(dp)-1])
+                
+        
+#         ans = float("inf")
+#         dp = {}
+#         dp[(0,0)] = tri[0][0]
+        
+#         for i in range(m):
+#             ans = min(ans, self.dfs(m-1, i, tri, dp))
+        
+#         return ans
+        
