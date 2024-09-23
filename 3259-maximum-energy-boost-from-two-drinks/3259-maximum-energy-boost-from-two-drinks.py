851dbf0eb7957d22d82d0a1c3a87c81a6224ca26
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-09-23 16:29:45 -0500

    Time: 1854 ms (24.84%), Space: 44.9 MB (31.78%) - LeetHub

--- 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.py
+++ 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.py
@@ -1,22 +1,35 @@
 class Solution:
-    def dfs(self, idx, nextDrink, A, B, dp):
-        if idx == 0:
-            if nextDrink == 0:
-                return A[idx]
-            return B[idx]
-        if (idx, nextDrink) in dp:
-            return dp[(idx, nextDrink)]
+    def maxEnergyBoost(self, A: List[int], B: List[int]) -> int:
+        m, n = len(A), 2
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0],dp[0][1] = A[0],B[0]
         
-        energy = A[idx] if nextDrink == 0 else B[idx]
-        drink = energy + self.dfs(idx-1, nextDrink, A, B, dp)
-        switch = self.dfs(idx-1, (not nextDrink), A, B, dp)
-        dp[(idx, nextDrink)] = max(drink, switch)
+        for i in range(1, m):
+            for j in range(n):
+                energy = A[i] if j == 0 else B[i]
+                dp[i][j] = max(energy+dp[i-1][j], dp[i-1][(not j)])
+
+        return max(dp[m-1][0], dp[m-1][1])
+
+# class Solution:
+#     def dfs(self, idx, nextDrink, A, B, dp):
+#         if idx == 0:
+#             if nextDrink == 0:
+#                 return A[idx]
+#             return B[idx]
+#         if (idx, nextDrink) in dp:
+#             return dp[(idx, nextDrink)]
         
-        return dp[(idx, nextDrink)]
+#         energy = A[idx] if nextDrink == 0 else B[idx]
+#         drink = energy + self.dfs(idx-1, nextDrink, A, B, dp)
+#         switch = self.dfs(idx-1, (not nextDrink), A, B, dp)
+#         dp[(idx, nextDrink)] = max(drink, switch)
+        
+#         return dp[(idx, nextDrink)]
         
     
-    def maxEnergyBoost(self, A: List[int], B: List[int]) -> int:
-        dp = {}
-        n = len(A)
-        return max(self.dfs(n-1, 0, A, B, dp), self.dfs(n-1, 1, A, B, dp))
+#     def maxEnergyBoost(self, A: List[int], B: List[int]) -> int:
+#         dp = {}
+#         n = len(A)
+#         return max(self.dfs(n-1, 0, A, B, dp), self.dfs(n-1, 1, A, B, dp))
         
