ac8c84930203e94129ff2f2cead13a0944780adc
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-09-22 21:56:27 -0500

    Time: 1928 ms (15.81%), Space: 32.8 MB (37.49%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,24 +1,45 @@
 class Solution:
-    def dfs(self, idx, target, nums, dp):
-        if target == 0:
-            return True
-        if idx == 0:
-            return (nums[idx] == target)
-        if (idx, target) in dp:
-            return dp[(idx, target)]
-        
-        pick = self.dfs(idx-1, target-nums[idx], nums, dp)
-        nonpick = self.dfs(idx-1, target, nums, dp)
-        dp[(idx, target)] = pick or nonpick
-        
-        return dp[(idx, target)]
-    
     def canPartition(self, nums: List[int]) -> bool:
         s = sum(nums)
         if s % 2 != 0:
             return False
-        dp = {}
-        return self.dfs(len(nums)-1, s//2, nums, dp)
+        
+        m, n = len(nums), (s//2)+1
+        dp = [[False for _ in range(n)] for _ in range(m)]
+        if nums[0] <= s//2:
+            dp[0][nums[0]] = True
+        
+        for idx in range(1, m):
+            for target in range(1, (s//2)+1):
+                pick = False
+                if target-nums[idx]>=0:
+                    pick = dp[idx-1][target-nums[idx]]
+                nonpick = dp[idx-1][target]
+                dp[idx][target] = (pick or nonpick)
+
+        return dp[m-1][n-1]
+
+# class Solution:
+#     def dfs(self, idx, target, nums, dp):
+#         if target == 0:
+#             return True
+#         if idx == 0:
+#             return (nums[idx] == target)
+#         if (idx, target) in dp:
+#             return dp[(idx, target)]
+        
+#         pick = self.dfs(idx-1, target-nums[idx], nums, dp)
+#         nonpick = self.dfs(idx-1, target, nums, dp)
+#         dp[(idx, target)] = pick or nonpick
+        
+#         return dp[(idx, target)]
+    
+#     def canPartition(self, nums: List[int]) -> bool:
+#         s = sum(nums)
+#         if s % 2 != 0:
+#             return False
+#         dp = {}
+#         return self.dfs(len(nums)-1, s//2, nums, dp)
 
 # class Solution:
 #     def dfs(self, idx, target, nums):
