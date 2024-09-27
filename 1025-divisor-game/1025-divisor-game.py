c50b4a25f9681671a43cfec4ea7f40a7b410bc62
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-09-27 14:26:55 -0500

    Time: 37 ms (47.77%), Space: 16.6 MB (40.03%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.py
@@ -0,0 +1,24 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return True if n % 2 == 0 else False
+#         dp = {}
+#         #Alice = 0, Bob = 1
+        
+#         def dfs(n, move):
+#             if n == 1:
+#                 return 1-move
+#             if (n, move) in dp:
+#                 return dp[(n, move)]
+            
+#             for x in range(1, n):
+#                 if n % x == 0:
+#                     dp[(n-x, 1-move)] = dfs(n-x, 1-move)
+#                     if dp[(n-x, 1-move)] == move:
+#                         return move
+            
+#             return 1-move
+        
+#         ans = dfs(n, 0)
+#         # print(dp)
+#         return True if ans == 0 else False
+        
