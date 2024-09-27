f4183b5cd6c8477402a02334b42974ae72881fdc
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-09-27 14:55:42 -0500

    Time: 73 ms (7.15%), Space: 17 MB (5.92%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,24 +1,49 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        # return True if n % 2 == 0 else False
-        dp = {}
-        #Alice = 0, Bob = 1
+        r, c = n+1, 2
+        dp = [[0 for _ in range(c)] for _ in range(r)]
+        dp[1][0] = 1
+        dp[1][1] = 0
         
-        def dfs(n, move):
-            if n == 1:
-                return 1-move
-            if (n, move) in dp:
-                return dp[(n, move)]
-            
-            for x in range(1, n):
-                if n % x == 0:
-                    dp[(n-x, 1-move)] = dfs(n-x, 1-move)
-                    if dp[(n-x, 1-move)] == move:
-                        return move
-            
-            return 1-move
+        for i in range(2, r):
+            for j in range(c):
+                
+                flag = False
+                for x in range(1, (i//2)+1):
+                    if i % x == 0:
+                        if dp[i-x][1-j] == j:
+                            dp[i][j] = j
+                            flag = True
+                            break
+                
+                if flag == False:
+                    dp[i][j] = 1-j
         
-        ans = dfs(n, 0)
-        # print(dp)
-        return True if ans == 0 else False
+        print(dp)
+        
+        return True if dp[n][0] == 0 else False
+
+# class Solution:
+#     def divisorGame(self, n: int) -> bool:
+#         # return True if n % 2 == 0 else False
+#         dp = {}
+#         #Alice = 0, Bob = 1
+        
+#         def dfs(n, move):
+#             if n == 1:
+#                 return 1-move
+#             if (n, move) in dp:
+#                 return dp[(n, move)]
+            
+#             for x in range(1, n):
+#                 if n % x == 0:
+#                     dp[(n-x, 1-move)] = dfs(n-x, 1-move)
+#                     if dp[(n-x, 1-move)] == move:
+#                         return move
+            
+#             return 1-move
+        
+#         ans = dfs(n, 0)
+#         # print(dp)
+#         return True if ans == 0 else False
         
