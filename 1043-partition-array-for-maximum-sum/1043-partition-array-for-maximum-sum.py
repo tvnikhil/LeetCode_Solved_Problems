e7687af4a7179d43f24b957af5ee6a02e05ec0f3
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-09-30 19:20:45 -0500

    Time: 1497 ms (10.80%), Space: 16.7 MB (38.86%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,22 +1,41 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        dp = {}
+        n = len(arr)
+        dp = [-1 for _ in range(n)]
+        dp[n-1] = arr[n-1]
         
-        def dfs(idx):
-            if idx >= len(arr):
-                return 0
-            if idx in dp:
-                return dp[idx]
-            
-            ans = -1
+        for i in range(n-2, -1, -1):
             for size in range(1, k+1):
-                st, end = idx, idx+size
-                if end > len(arr):
+                st, end = i, i+size
+                if end > n:
                     break
-                ans = max(ans, (max(arr[st:end])*size) + dfs(end))
+                elif end == n:
+                    res = max(arr[st:end])*size
+                else:
+                    res = max(arr[st:end])*size + dp[end]
+                dp[i] = max(dp[i], res)
+        
+        return dp[0]
+
+# class Solution:
+#     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+#         dp = {}
+        
+#         def dfs(idx):
+#             if idx >= len(arr):
+#                 return 0
+#             if idx in dp:
+#                 return dp[idx]
             
-            dp[idx] = ans
-            return ans
+#             ans = -1
+#             for size in range(1, k+1):
+#                 st, end = idx, idx+size
+#                 if end > len(arr):
+#                     break
+#                 ans = max(ans, (max(arr[st:end])*size) + dfs(end))
             
-        return dfs(0)
+#             dp[idx] = ans
+#             return ans
+        
+#         return dfs(0)
         
