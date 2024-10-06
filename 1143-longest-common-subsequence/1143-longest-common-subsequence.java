a2f41f6cce56aa5a9d8212523bc48cd0ce0e5ef4
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-10-06 10:19:19 -0500

    Time: 80 ms (5.05%), Space: 80.2 MB (5.07%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,21 +1,21 @@
 class Solution {
     
-    int lcs(int idx1, int idx2, String s1, String s2, List<List<Integer>> dp) {
-        if (idx1 < 0 || idx2 < 0)
-            return 0;
-        if (dp.get(idx1).get(idx2) != -1)
-            return dp.get(idx1).get(idx2);
-        if (s1.charAt(idx1) == s2.charAt(idx2)) {
-            dp.get(idx1).set(idx2, 1+lcs(idx1-1, idx2-1, s1, s2, dp));
-            return dp.get(idx1).get(idx2);
-        }
-        dp.get(idx1).set(idx2, Math.max(lcs(idx1-1, idx2, s1, s2, dp), lcs(idx1, idx2-1, s1, s2, dp)));
-        return dp.get(idx1).get(idx2);
-    }
+    // int lcs(int idx1, int idx2, String s1, String s2, List<List<Integer>> dp) {
+    //     if (idx1 < 0 || idx2 < 0)
+    //         return 0;
+    //     if (dp.get(idx1).get(idx2) != -1)
+    //         return dp.get(idx1).get(idx2);
+    //     if (s1.charAt(idx1) == s2.charAt(idx2)) {
+    //         dp.get(idx1).set(idx2, 1+lcs(idx1-1, idx2-1, s1, s2, dp));
+    //         return dp.get(idx1).get(idx2);
+    //     }
+    //     dp.get(idx1).set(idx2, Math.max(lcs(idx1-1, idx2, s1, s2, dp), lcs(idx1, idx2-1, s1, s2, dp)));
+    //     return dp.get(idx1).get(idx2);
+    // }
     
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int longestCommonSubsequence(String s1, String s2) {
         List<List<Integer>> dp = new ArrayList<>();
-        int m = text1.length(), n = text2.length();
+        int m = s1.length(), n = s2.length();
         for (int i = 0; i < m; i++) {
             dp.add(new ArrayList<>());
             for (int j = 0; j < n; j++) {
@@ -23,6 +23,27 @@ class Solution {
             }
         }
         
-        return lcs(m-1, n-1, text1, text2, dp);
+        for (int idx1 = 0; idx1 < m; idx1++) {
+            for (int idx2 = 0; idx2 < n; idx2++) {
+                if (s1.charAt(idx1) == s2.charAt(idx2)) {
+                    if (idx1-1>=0 && idx2-1>=0)
+                        dp.get(idx1).set(idx2, 1+dp.get(idx1-1).get(idx2-1));
+                    else
+                        dp.get(idx1).set(idx2, 1);
+                }
+                else {
+                    int case1 = 0, case2 = 0;
+                    if (idx1-1>=0)
+                        case1 = dp.get(idx1-1).get(idx2);
+                    if (idx2-1>=0)
+                        case2 = dp.get(idx1).get(idx2-1);
+                    dp.get(idx1).set(idx2, Math.max(case1, case2));
+                }
+            }
+        }
+        
+        return dp.get(m-1).get(n-1);
+        
+        // return lcs(m-1, n-1, s1, s2, dp);
     }
 }
