b44b87ace047ce9795bf5d955a55efb4cb367a7f
Nikhil Thanikella <83802691+tvnikhil@users.noreply.github.com>
2024-10-08 13:48:30 -0500

    Time: 1192 ms (42.92%), Space: 16.6 MB (92.20%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,16 +1,22 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         m, n = len(text1), len(text2)
-        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        # dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        prev = [0 for _ in range(n+1)]
+        curr = list(prev)
         
         for idx1 in range(1, m + 1):
             for idx2 in range(1, n + 1):
                 if text1[idx1 - 1] == text2[idx2 - 1]:
-                    dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1]
+                    # dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1]
+                    curr[idx2] = 1 + prev[idx2-1]
                 else:
-                    dp[idx1][idx2] = max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1])
+                    # dp[idx1][idx2] = max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1])
+                    curr[idx2] = max(prev[idx2], curr[idx2 - 1])
+            prev = list(curr)
         
-        return dp[m][n]
+        return curr[n]
+        # return dp[m][n]
     
     def longestPalindromeSubseq(self, s: str) -> int:
         return self.longestCommonSubsequence(s, s[::-1])
